c586724fc775ae02d6072d37c1107314d08fcd08
Keith Turner <turner@keithturner.com>
2018-02-27 18:38:43 -0700

    Update arrays

--- arrays.php
+++ arrays.php
@@ -1,6 +1,7 @@
+
 <?php
 
-
+/*
 $days = array("Monday", "Tuesday", "Wednesday");
 
 foreach($days as $value) {echo "&bull; $value";}
@@ -12,8 +13,30 @@ foreach ($months as $key => $value)
  { echo "<dt>$key<dd>$value";}
  echo '</dl>';
 
+*/
 
+# Sorting arrays
 
+$cars = array('Dodge' => 'Viper', 'Chevrolet' => 'Camero', 'Ford' => 'Mustang');
 
+echo '<dl><dt>Original Element Order :<dd>';
+foreach($cars as $key => $value)
+{echo '&bull;' , $key. '' .$value;}
+
+asort($cars);
+
+echo '<dt>Sorted into the Value Order :<dd>';
+
+foreach($cars as $key => $value)
+{echo '&bull; ', $key. ' ' .$value;}
+
+ksort($cars);
+
+echo '<dt>Sorted into the Key Order :<dd>';
+
+foreach($cars as $key => $value)
+{echo '&bull; ', $key. ' ' .$value;}
+
+echo '</dl>';
 
  ?>
